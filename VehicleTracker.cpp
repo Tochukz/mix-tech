6f5c18777d18d8948216c871234c91c07f208a41
Tochukwu Nwachukwu <truetochukz@gmail.com>
2022-03-18 02:08:18 +0200

    Uptimized the shortest distance calculation

--- VehicleTracker.cpp
+++ VehicleTracker.cpp
@@ -31,17 +31,17 @@ private:
     float Longitude;
     int64_t RecordedTimeUTC;
    
-    vector<Position> Positions;
+    vector<Position> PositionsOfInterest;
+
+    vector<Position> OtherPositions;
 
 public: 
 
-    VehicleTracker() {
-
-    }
+    VehicleTracker() {}
 
     ~VehicleTracker() {}
 
-    void ReadDataFile(string  filename)
+    void ReadDataFile(string  filename, float minLat, float maxLat, float minLng, float maxLng)
     {
     
        ifstream binaryFile;
@@ -56,10 +56,16 @@ public:
        {
            Position position;
            binaryFile.read(reinterpret_cast<char*>(&position), positionSize);
-           Positions.push_back(position);
+           if (position.Latitude >= minLat && position.Latitude <= maxLat && position.Longitude >= minLng && position.Longitude <= maxLng)
+           {
+               PositionsOfInterest.push_back(position);
+           }
+           else 
+           {
+              OtherPositions.push_back(position);
+           }
+          
        }
-       
-       printf("Total Record %d\n", Positions.size());
        binaryFile.close();
     }
 
@@ -67,23 +73,31 @@ public:
     {
         const float maxDistance = 7600;
         vector<Vehicle> vehicles;
-        int i = 0;
         for(Position val : vehiclePositions)
         {
             Position position;
             Vehicle vehicle { position, maxDistance };
             vehicles.push_back(vehicle);
-            i++;
         }
 
-        for(Position position: Positions) {
+        vector<Position> SearchPool;
+        if (PositionsOfInterest.size() > 0)
+        {
+            SearchPool = PositionsOfInterest;
+        }
+        else 
+        {
+            SearchPool = OtherPositions;
+        }
+        
+        for(Position position: SearchPool) {
             for(size_t i = 0; i < vehiclePositions.size(); i++) {
-               double distance = CalculateDistance(position, vehiclePositions[i]);
-               if (distance < vehicles[i].Distance)
-               {
-                   Vehicle vehicle { position, distance };
-                   vehicles[i] = vehicle;
-               }            
+                double distance = CalculateDistance(position, vehiclePositions[i]);
+                if (distance < vehicles[i].Distance)
+                {
+                    Vehicle vehicle { position, distance };
+                    vehicles[i] = vehicle;
+                }           
             }
         }
         return vehicles;
@@ -110,15 +124,10 @@ int main()
     using std::chrono::duration_cast;
     using std::chrono::milliseconds;
 
-    string filename = "VehiclePositions.dat";
-    VehicleTracker* tracker = new VehicleTracker();
-    
-    auto startTime = high_resolution_clock::now();
-    tracker->ReadDataFile(filename);
-    auto endTime = high_resolution_clock::now();
-    auto duration = duration_cast<milliseconds>(endTime - startTime);
-    printf("Datafile read execution time: %d ms \n", duration);
-
+    float minLat = 180;
+    float maxLat = -180;
+    float minLng = 180;
+    float maxLng = -180;
     float coordinates[10][2] = {
         {34.544909, -102.100843},
         {32.345544, -99.123124},
@@ -131,36 +140,57 @@ int main()
         {33.535339, -94.792232},
         {32.234235, -100.222222}
     };
+
     vector<Position> positions;    
     for (auto point: coordinates)
     {   
         Position position = {1, "Position ", point[0], point[1]};
         positions.push_back(position);
+
+        if (position.Latitude < minLat)
+        {
+            minLat = position.Latitude;
+        }
+        if (position.Latitude > maxLat)
+        {
+            maxLat = position.Latitude;
+        }
+
+        if (position.Longitude < minLng)
+        {
+            minLng = position.Longitude;
+        }
+        if (position.Longitude > maxLng)
+        {
+            maxLng = position.Longitude;
+        }
     }
 
+    minLat -= 5;
+    maxLat += 5;
+    minLng -= 5;
+    maxLng += 5;
+
+    string filename = "VehiclePositions.dat";
+    VehicleTracker* tracker = new VehicleTracker();
+    
+    auto startTime = high_resolution_clock::now();
+    tracker->ReadDataFile(filename, minLat, maxLat, minLng, maxLng);
+    auto endTime = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(endTime - startTime);
+    printf("Data file read execution time: %d ms \n", duration);
+
+    
     auto startT = high_resolution_clock::now();
     vector<Vehicle> vehicles = tracker->GetShortestDistance(positions);
     auto endT = high_resolution_clock::now();
     auto timeSpan = duration_cast<milliseconds>(endT - startT);
     printf("Closest position calculation execution time: %d ms \n", timeSpan);
 
-    printf("Shortest Distances: \n");
+    printf("\tCoordinate \t \t \t Nearest Position \n");
     for (int i = 0; i < vehicles.size(); i++)
     {
-       printf("Lat: %f, Lng: %f => Lat: %f, Lng: %f, Dist: %f Km \n", positions[i].Latitude, positions[i].Longitude, vehicles[i].Position.Latitude, vehicles[i].Position.Longitude,vehicles[i].Distance);
-    }
-
-   /*
-    auto startX = high_resolution_clock::now();
-
+       printf("%d. (%f, %f) \t (%f, %f)  %f Km \n", i+1, positions[i].Latitude, positions[i].Longitude, vehicles[i].Position.Latitude, vehicles[i].Position.Longitude,vehicles[i].Distance);
+    } 
     printf("Total execution time: %d ms \n", (duration+ timeSpan));
-    Position pointA { 1 , "Position 1",  53.32055555555556, -1.7297222222222221};
-    Position pointB { 2, "Position 2", 53.31861111111111, -1.6997222222222223};
-    double distance = tracker->CalculateDistance(pointA, pointB);
-    printf("Distance = %f Km \n", distance);
-
-    auto endX = high_resolution_clock::now();
-    auto timeSpanX = duration_cast<milliseconds>(endX - startX);
-    printf("Calculation execution time: %d ms \n", timeSpanX);
-    */
 }
